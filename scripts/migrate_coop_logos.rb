0309c0390e1bd0ccd89d6aecf23d3147abe2a77b
Chris Fourie <robot_elbow@protonmail.com>
2023-02-22 20:26:14 +0200

    modified footer

--- scripts/migrate_coop_logos.rb
+++ scripts/migrate_coop_logos.rb
@@ -6,6 +6,7 @@ Coop.all.each do |coop|
   current_fn = File.join(base_dir, coop.logo)
   ext = File.extname(current_fn)
   new_fn = File.join(File.dirname(__FILE__), '..', 'images', 'coops', coop.slug + ext)
+  
 
   FileUtils.cp(current_fn, new_fn)
 end
